c7dff9094f7503a686abf341b91d50103f11fe10
Stuart Wheater <stuart.wheater@arjuna.com>
2015-01-29 23:42:54 +0000

    Initial navigation

--- src/main/webapp/app.js
+++ src/main/webapp/app.js
@@ -3,9 +3,9 @@ var agreementApp = angular.module('agreementApp', []);
 agreementApp.controller('agreementCtrl', function ($scope)
 {
      $scope.features = [
-         { 'name': 'Field 1', 'text': 'Test text 1'},
-         { 'name': 'Field 2', 'text': 'Test text 2'},
-         { 'name': 'Field 3', 'text': 'Test text 3'},
-         { 'name': 'Field 3', 'text': 'Test text 4'}
+         { 'name': 'Field 1', 'text': 'Test text 1', 'status': 'active' },
+         { 'name': 'Field 2', 'text': 'Test text 2', 'status': 'proposed' },
+         { 'name': 'Field 3', 'text': 'Test text 3', 'status': 'active' },
+         { 'name': 'Field 3', 'text': 'Test text 4', 'status': 'proposed' },
      ];
 });
