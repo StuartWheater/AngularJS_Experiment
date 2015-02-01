a33806b1baa3dce8c6f2bab1b5279b875c1865ad
Stuart Wheater <stuart.wheater@gmail.com>
2015-02-01 17:45:00 +0000

    New layout

--- src/main/webapp/agreements/agreements.js
+++ src/main/webapp/agreements/agreements.js
@@ -1,3 +1,6 @@
-agreementsApp.controller('agreementsCtrl', function ($scope)
+angular.module('agreements', []);
+
+angular.module('agreements').controller('agreementsCtrl', ['$scope', function ($scope)
 {
-});
+    $scope.features = [ {"name": "Name", "text": "Text", "status": "active"} ];
+}]);
--- src/main/webapp/app.js
+++ src/main/webapp/app.js
@@ -1,5 +1,33 @@
-var agreementApp = angular.module('agreementApp', []);
+var mainApp = angular.module('mainApp', ['agreements', 'agreement', 'agreementtemplates', 'agreementtemplate']);
 
-agreementApp.controller('mainCtrl', function ($scope)
+angular.module('mainApp').controller('mainCtrl', ['$scope', function ($scope)
 {
-});
+    $scope.features = [ {"name": "Name", "text": "Text", "status": "active"} ];
+}]);
+
+angular.module('mainapp').config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider)
+{
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider.
+        state('agreements',
+        {
+            url: '/',	  
+            templateUrl: 'agreements/agreements.html'
+	}).
+        state('agreement',
+	{
+            url: '/',	  
+            templateUrl: 'agreement/agreement.html'
+	}).
+        state('agreementtemplates',
+	{
+            url: '/',	  
+            templateUrl: 'agreementtemplates/agreementtemplates.html'
+	}).
+        state('agreementtemplate',
+	{
+            url: '/',	  
+            templateUrl: 'agreementtemplate/agreementtemplate.html'
+	});
+}]);
