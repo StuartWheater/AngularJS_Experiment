43c0801b61cb8b50508b591c2f28152e156ed17f
Stuart Wheater <stuart.wheater@gmail.com>
2015-02-02 01:31:32 +0000

    Fixed 'and' to 'as'

--- src/main/webapp/agreements/agreements.js
+++ src/main/webapp/agreements/agreements.js
@@ -2,5 +2,9 @@ angular.module('agreementsModule', []);
 
 angular.module('agreementsModule').controller('agreementsCtrl', ['$scope', function ($scope)
 {
-    $scope.features = [ {"name": "Name", "text": "Text", "status": "active"} ];
+    $scope.features =
+    [
+       {"name": "Name 1", "text": "Text 1", "status": "active"},
+       {"name": "Name 2", "text": "Text 2", "status": "active"}
+    ];
 }]);
