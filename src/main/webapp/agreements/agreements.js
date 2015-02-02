75a3329ddc938a9d6f296aafd19989bf95f7e7d2
Stuart Wheater <stuart.wheater@arjuna.com>
2015-02-02 13:08:32 +0000

    Experiment with paging

--- src/main/webapp/agreements/agreements.js
+++ src/main/webapp/agreements/agreements.js
@@ -2,9 +2,54 @@ angular.module('agreementsModule', []);
 
 angular.module('agreementsModule').controller('agreementsCtrl', ['$scope', function ($scope)
 {
-    $scope.features =
+    $scope.currentPage = 1;
+    $scope.pageSize    = 10;
+
+    $scope.agreements =
     [
-       {"name": "Name 1", "text": "Text 1", "status": "active"},
-       {"name": "Name 2", "text": "Text 2", "status": "active"}
+       {"name": "Name  1", "text": "Text Text Text Text 01", "status": "active"},
+       {"name": "Name  2", "text": "Text Text Text Text 02", "status": "active"},
+       {"name": "Name  3", "text": "Text Text Text Text 03", "status": "active"},
+       {"name": "Name  4", "text": "Text Text Text Text 04", "status": "active"},
+       {"name": "Name  5", "text": "Text Text Text Text 05", "status": "active"},
+       {"name": "Name  6", "text": "Text Text Text Text 06", "status": "active"},
+       {"name": "Name  7", "text": "Text Text Text Text 07", "status": "active"},
+       {"name": "Name  8", "text": "Text Text Text Text 08", "status": "active"},
+       {"name": "Name  9", "text": "Text Text Text Text 09", "status": "active"},
+       {"name": "Name 10", "text": "Text Text Text Text 10", "status": "active"},
+       {"name": "Name 11", "text": "Text Text Text Text 11", "status": "active"},
+       {"name": "Name 12", "text": "Text Text Text Text 12", "status": "active"},
+       {"name": "Name 13", "text": "Text Text Text Text 13", "status": "active"},
+       {"name": "Name 14", "text": "Text Text Text Text 14", "status": "active"},
+       {"name": "Name 15", "text": "Text Text Text Text 15", "status": "active"},
+       {"name": "Name 16", "text": "Text Text Text Text 16", "status": "active"},
+       {"name": "Name 17", "text": "Text Text Text Text 17", "status": "active"},
+       {"name": "Name 18", "text": "Text Text Text Text 18", "status": "active"},
+       {"name": "Name 19", "text": "Text Text Text Text 19", "status": "active"},
+       {"name": "Name 20", "text": "Text Text Text Text 20", "status": "active"},
+       {"name": "Name 21", "text": "Text Text Text Text 21", "status": "active"},
+       {"name": "Name 22", "text": "Text Text Text Text 22", "status": "active"},
+       {"name": "Name 23", "text": "Text Text Text Text 23", "status": "active"},
+       {"name": "Name 24", "text": "Text Text Text Text 24", "status": "active"}
     ];
+
+    this.setCurrentPage = function(currentPage)
+    {
+    	$scope.currentPage = currentPage;
+    };
+
+    this.isCurrentPage = function(currentPage)
+    {
+    	return $scope.currentPage == currentPage;
+    };
+
+    this.decreaseCurrentPage = function()
+    {
+    	$scope.currentPage--;
+    };
+
+    this.increaseCurrentPage = function()
+    {
+    	$scope.currentPage++;
+    };
 }]);
